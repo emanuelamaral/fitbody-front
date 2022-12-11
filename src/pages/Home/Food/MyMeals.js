02ba9b1ecc1f9bff69d3ac7e6a5af00c834f71d1
emanuelamaral <amoz_c.p.t@hotmail.com>
2022-12-11 15:44:39 -0300

    Add modal option

--- src/pages/Home/Food/MyMeals.js
+++ src/pages/Home/Food/MyMeals.js
@@ -7,7 +7,8 @@ import {
    TouchableOpacity, 
    Text, 
    StyleSheet,
-   FlatList} from "react-native";
+   FlatList,
+   Modal} from "react-native";
 
 import CleanHeader from "../../../components/CleanHeader";
 import { AuthContext } from "../../../contexts/Authentication/Auth";
@@ -15,6 +16,8 @@ import { AuthContext } from "../../../contexts/Authentication/Auth";
 export default function MyMeals({navigation}){
   
   const {mealsProfile, foodsByUserProfile, getAllFoodByUserProfile} = useContext(AuthContext);
+
+  const [modalEditMeal, setModalEditMeal] = useState(false)
 
   async function verifyFiels(){
     food = foodsByUserProfile
@@ -32,14 +35,16 @@ export default function MyMeals({navigation}){
     navigation.navigate("AddFood")
   }
 
-  
+  function editMeals(){
+    navigation.navigate('EditMeal')
+  }
 
     return(
 
       <View style={styles.container}>
         <CleanHeader/>
         <View style={styles.containerExplain}>
-          <Text style={styles.txtExplain}>Clique na refeição para adicionar os alimentos à ela</Text>
+          <Text style={styles.txtExplain}>Clique na refeição que deseja editar</Text>
         </View>
 
         <View style={styles.containerMain}>
@@ -63,7 +68,53 @@ export default function MyMeals({navigation}){
                 }}
             />
           </View>
+          <View style={styles.centeredView}>
+            <Modal
+              animationType="slide"
+              transparent={true}
+              visible={modalEditMeal}
+              >
+              <View style={styles.centeredView}>
+                  <View style={styles.modalView}>
+
+                      <Text style={styles.txtOptionModal}>Escolha a ação que deseja fazer</Text>
+
+                      <View style={styles.viewOptionModal}>
+                        <TouchableOpacity 
+                          style={styles.btnOptionModal}
+                          onPress={() => editMeals()}>
+                          <Text style={styles.txtOptionBtnModal}>Editar</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity style={styles.btnOptionModal}>
+                          <Text style={styles.txtOptionBtnModal}>Remover</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity style={styles.btnOptionModal}>
+                          <Text style={styles.txtOptionBtnModal}>Adicionar</Text>
+                        </TouchableOpacity>
+                      </View>
+
+                      <TouchableOpacity
+                          style={[styles.buttonBackSheet, styles.buttonBackSheetColor]}
+                          onPress={() => setModalEditMeal(!modalEditMeal)}
+                      >
+                          <Text style={styles.textStyle}>Voltar</Text>
+                      </TouchableOpacity>
+
+                  </View>
+              </View>
+            </Modal>
+          </View>
+
           <View style={styles.containerAddFood}>
+              <TouchableOpacity
+                style={styles.btnEditMeal}
+                onPress={() => setModalEditMeal(true)}
+              >
+                <Text style={styles.txtAddFood}>Editar Refeições</Text>
+              </TouchableOpacity>
+
               <TouchableOpacity
                   style={styles.btnAddFood}
                   onPress={() => verifyFiels()}
@@ -88,7 +139,8 @@ const styles = StyleSheet.create({
     marginTop: 10,
     marginStart: 10,
     marginEnd: 10,
-    marginBottom: 10
+    marginBottom: 10,
+
   },
 
   containerExplain:{
@@ -102,7 +154,7 @@ const styles = StyleSheet.create({
 
   containerFlatMeals:{
     // backgroundColor: '#000',
-    height: 400
+    height: 300
   },
 
   containerTitle:{
@@ -148,8 +200,8 @@ const styles = StyleSheet.create({
   },
   containerAddFood:{
     // backgroundColor: '#000',
-    marginTop: 53,
-    height: 80,
+    height: 220,
+    marginTop: 30,
     justifyContent: 'center'
   },
 
@@ -159,7 +211,7 @@ const styles = StyleSheet.create({
     borderRadius: 25,
     marginStart: 20,
     marginEnd: 20,
-    marginTop: 10,
+    // marginTop: 10,
     justifyContent: 'center',
     alignItems: 'center'
   },
@@ -168,6 +220,108 @@ const styles = StyleSheet.create({
     color: '#FFF',
     fontSize: 20,
     fontWeight: 'bold'
-  }
+  },
+
+  btnEditMeal:{
+    height: 60,
+    backgroundColor: '#136f8a',
+    borderRadius: 25,
+    marginStart: 20,
+    marginEnd: 20,
+    marginBottom: 15,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  txtOptionModal:{
+    fontSize: 20,
+    marginTop: 10
+  },
+
+  viewOptionModal:{
+    // backgroundColor: '#000',
+    height: 250,
+    width: 250,
+    margin: 10,
+    // alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  btnOptionModal:{
+    height: 60,
+    backgroundColor: '#ef233c',
+    borderRadius: 25,
+    marginStart: 20,
+    marginEnd: 20,
+    marginBottom: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    elevation: 2,
+  },
+
+  txtOptionBtnModal:{
+    color: '#FFF',
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+},
+
+  modalView: {
+    // margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    // padding: 35,
+    height: 400,
+    width: 350,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+        width: 0,
+        height: 2
+  },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+
+  buttonSaveSheet: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    marginTop: 65,
+    marginEnd: 120,
+
+  },
+
+  buttonOpenSaveSheet: {
+    backgroundColor: "#F194FF",
+  },
+
+  buttonBackSheet:{
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    top: 40
+    // marginStart: 220,
+    // bottom: 40
+    // // marginTop: 65
+  },
+
+  buttonBackSheetColor:{
+    backgroundColor: "#F194FF",
+  },
+
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
 
 })
